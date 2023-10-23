6e42b1930b43920344acc925c2f615af02f162c0
yousrahibasara <yousrahoumma92@gmail.com>
2023-10-23 05:10:22 -0700

    5-raise_exception.py

---
+++ 0x05-python-exceptions/5-raise_exception.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def raise_exception():
+    raise TypeError
+
+
+
+
+vi 6-raise_exception_msg.py
+
+#!/usr/bin/python3
+def raise_exception_msg(message=""):
+    raise NameError(message)
+
