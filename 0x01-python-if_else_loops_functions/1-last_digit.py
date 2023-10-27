29c606ef14cb7fe21b5d75eb1920ed78c9b69943
yousrahibasara <yousrahoumma92@gmail.com>
2023-10-27 04:34:33 -0700

    0x01. Python - if/else, loops, functions

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,15 +1,17 @@
-
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-if number < 0:
-    last_num = number % -10
-elif number >= 0:
-    last_num = number % 10
-if last_num > 5:
-    print(f"Last digit of {number} is {last_num} and is greater than 5")
-elif last_num == 0:
-    print(f"Last digit of {number} is {last_num} and is 0")
+if number >= 0:
+    last_digit = number % 10
 else:
-    print(f"Last digit of {number} is {last_num} and is less than 6 and not 0")
+    last_digit = ((number * -1) % 10) * -1
+
+message = "Last digit of %d is %d and is" % (number, last_digit)
+
+if last_digit == 0:
+    print(message, "0")
+elif last_digit > 5:
+    print(message, "greater than 5")
+else:
+    print(message, "less than 6 and not 0")
 
--- 0x01-python-if_else_loops_functions/lists.h
+++ 0x01-python-if_else_loops_functions/lists.h
@@ -2,7 +2,6 @@
 #define LISTS_H
 
 #include <stdlib.h>
-
 /**
  * struct listint_s - singly linked list
  * @n: integer
@@ -13,14 +12,15 @@
  */
 typedef struct listint_s
 {
-        int n;
-        struct listint_s *next;
+    int n;
+    struct listint_s *next;
 } listint_t;
 
 size_t print_listint(const listint_t *h);
-listint_t *add_nodeint(listint_t **head, const int n);
+listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
-int check_cycle(listint_t *list);
+
+listint_t *insert_node(listint_t **head, int number);
 
 #endif /* LISTS_H */
 
